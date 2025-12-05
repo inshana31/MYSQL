92865637495b5bf31a3a3246f754c26ee76c3438
Inshana <inshana31@gmail.com>
2025-12-05 10:08:03 +0530

    sql

---
+++ CLASSWORK/day2.sql
@@ -0,0 +1,4 @@
+SELECT * FROM `students` WHERE age>20;
+SELECT * FROM `students` WHERE Grade=90;
+SELECT * FROM `students` WHERE department ='physics';
+SELECT * FROM `students` WHERE Grade BETWEEN 70 AND 90;
---
+++ HOMEWORK/day2.sql
@@ -0,0 +1,5 @@
+INSERT INTO `library`( `Title`, `Author`, `Price`, `Genre`) VALUES ( 'The Great Gatsby','Haleema','500','history'), ( ' Great ','Dan Brown','500','social'), ( 'The man','hema','250','science'), ( 'spiderman ','Dan Brown','100','fiction'), ('halloween','Dan Brown','600','fiction');
+SELECT * FROM library WHERE price>400;
+SELECT* FROM library WHERE genre= history; 
+SELECT library WHERE title= The Great Gatsby;
+SELECT* FROM library WHERE<> Dan Brown;
